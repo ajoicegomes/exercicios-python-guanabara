32df8293af3ed0ef376f81eaeeb6d5f8eeab830b
Jóice <joicermgomes@outlook.com>
2025-01-17 09:06:05 -0300

    olamundo.py
    Meu primeiro código em Python.

---
+++ ex001.py
@@ -0,0 +1,8 @@
+# Exibe a mensagem 'Olá, Mundo!' diretamente no console
+print('Olá, Mundo!')
+
+# Atribui a string 'Olá Mundo' à variável msg
+msg = 'Olá Mundo'
+
+# Exibe o conteúdo da variável msg, que é 'Olá Mundo'
+print(msg)
