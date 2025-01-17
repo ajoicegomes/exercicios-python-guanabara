ba91ef90935d91a9c53f82f96c5fba54260a9639
Jóice <joicermgomes@outlook.com>
2025-01-17 09:18:06 -0300

    saudacao_personalizada.py
    Adiciona interação com o usuário para solicitar o nome e exibir uma mensagem personalizada

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Solicita ao usuário que digite seu nome e armazena o valor na variável 'nome'
+nome = input('Digite seu nome: ')
+
+# Exibe uma mensagem personalizada utilizando o nome fornecido pelo usuário
+print(f'É um prazer te conhecer, {nome}!')
