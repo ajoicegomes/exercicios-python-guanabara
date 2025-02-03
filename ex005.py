559f251d221ce9112466a2c5195b804a8ba56ea2
Jóice <joicermgomes@outlook.com>
2025-02-03 08:22:28 -0300

    antecessor_sucessor.py
    Script para exibir antecessor e sucessor de um número.

---
+++ ex005.py
@@ -0,0 +1,5 @@
+# Solicita ao usuário que digite um número inteiro e armazena na variável 'n'
+n = int(input('Digite um número inteiro: '))
+
+# Exibe o número informado pelo usuário, seu antecessor (n-1) e sucessor (n+1)
+print(f'O número {n} tem como seu antecessor {n-1} e sucessor {n+1}')
