da304cf30f72c1c2d3b62c57ae29e34cb19bd4fd
Jóice <joicermgomes@outlook.com>
2025-02-03 08:24:09 -0300

    dobro_triplo_raiz.py
    Script para exibir o dobro, triplo e raiz quadrada de um número.

---
+++ ex006.py
@@ -0,0 +1,12 @@
+# Solicita ao usuário que digite um número e armazena em 'n'
+n = int(input('Digite um número: '))
+
+# Exibe o dobro do número
+print(f'O dobro de {n} vale {n*2}')
+
+# Exibe o triplo do número
+print(f'O triplo de {n} vale {n*3}')
+
+# Exibe a raiz quadrada do número
+print(f'A raiz quadrada de {n} vale {pow(n, 1/2)}')
+
